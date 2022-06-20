9365fd8cfee39cbccf2626ac2562f4c9f121659a
AzadehNazemi <93635916+AzadehNazemi@users.noreply.github.com>
2022-06-20 10:38:40 +0800

    Update anomalyDetection_IsolationForest_colorHistogram.py

--- anomalyDetection_IsolationForest_colorHistogram.py
+++ anomalyDetection_IsolationForest_colorHistogram.py
@@ -1,4 +1,33 @@
+from sklearn.ensemble import IsolationForest
+import sys
+import pickle
+from imutils import paths
+import cv2
+import numpy as np
 
+def quantify_image(image, bins=(4, 6, 3)):
+	hist = cv2.calcHist([image], [0, 1, 2], None, bins,
+		[0, 180, 0, 256, 0, 256])
+	hist = cv2.normalize(hist, hist).flatten()
+
+	return hist
+
+def load_dataset(datasetPath, bins):
+	imagePaths = list(paths.list_images(datasetPath))
+	data = []
+
+	for imagePath in imagePaths:
+		image = cv2.imread(imagePath)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+		features = quantify_image(image, bins)
+		data.append(features)
+
+	return np.array(data)
+    
+'''
+TRAIN
+'''
 
 data = load_dataset(sys.argv[1], bins=(3, 3, 3))
 
